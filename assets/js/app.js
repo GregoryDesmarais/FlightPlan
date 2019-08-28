f96ba20269a4ed725c4f7af17bdd5eab16bc8703
Scott Daniels <sdaniels6363@gmail.com>
2019-08-28 19:05:20 -0400

    initial function for eventbrite api call

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,33 @@
+// api call function 
+function eventbriteAPI (destination, startDate, endDate){
+  
+  if (destination){
+    console.log(destination);
+  };
+  if (startDate){
+    startDate = moment(startDate).format("YYYY-MM-DDThh:mm:ss");
+  };
+  if (endDate){
+    endDate = moment(endDate).format("YYYY-MM-DDThh:mm:ss");
+  }
+  
+  var queryURL = `https://www.eventbriteapi.com/v3/events/search?start_date.range_start=${startDate}&start_date.range_end=${endDate}&location.address=${destination}`;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    beforeSend: function (request)
+    {
+        request.withCredentials = true;
+        request.setRequestHeader("Authorization", "Bearer QPEWGCGG3AMHB3TDR5S2");
+        // request.setRequestHeader()
+    },
+  }).then(function (response) {
+    var events = response.events;
+
+  });
+}
+
 
 $(document).ready(function () {
   $('select').formSelect();
