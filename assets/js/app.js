b23d69f4de5a080d4e1655c6c36875ea95d42566
GDesmarais <46517644+GregoryDesmarais@users.noreply.github.com>
2019-09-07 10:21:56 -0400

    Create seperatre data.js file for storing various objects.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,32 +2,6 @@ var pageNo = 1;
 var pageCount;
 var events = [];
 
-//All available categories from Eventbrite's API.
-var categories = {
-    101: "Business & Professional",
-    102: "Science & Technology",
-    103: "Music",
-    104: "Film, Media & Entertainment",
-    105: "Performing & Visual Arts",
-    106: "Fashion & Beauty",
-    107: "Health & Wellness",
-    108: "Sports & Fitness",
-    109: "Travel & Outdoor",
-    110: "Food & Drink",
-    111: "Charity & Causes",
-    112: "Government & Politics",
-    113: "Community & Culture",
-    114: "Religion & Spirituality",
-    115: "Family & Education",
-    116: "Seasonal & Holiday",
-    117: "Home & Lifestyle",
-    118: "Auto, Boat & Air",
-    119: "Hobbies & Special Interest",
-    120: "School Activities",
-    199: "Other",
-    null: "None"
-};
-
 //Dynamically add filter categories to Filter Dropdown.
 $("#filter").empty();
 for (x in categories) {
@@ -124,18 +98,6 @@ $(document).ready(function () {
 
 
 });
-
-
-// $(document).ready(function(){
-//   for ( i = 0; i < events.length;i++){
-//     var event = events[i];
-//     var free = event.is_free;
-//     var name = event.name;
-//     var url = event.url;
-//     // var tableEntry = 
-//   }
-// });
-
 
 $(document).ready(function () {
     $('select').formSelect();
---
+++ assets/js/data.js
@@ -0,0 +1,25 @@
+//All available categories from Eventbrite's API.
+var categories = {
+    101: "Business & Professional",
+    102: "Science & Technology",
+    103: "Music",
+    104: "Film, Media & Entertainment",
+    105: "Performing & Visual Arts",
+    106: "Fashion & Beauty",
+    107: "Health & Wellness",
+    108: "Sports & Fitness",
+    109: "Travel & Outdoor",
+    110: "Food & Drink",
+    111: "Charity & Causes",
+    112: "Government & Politics",
+    113: "Community & Culture",
+    114: "Religion & Spirituality",
+    115: "Family & Education",
+    116: "Seasonal & Holiday",
+    117: "Home & Lifestyle",
+    118: "Auto, Boat & Air",
+    119: "Hobbies & Special Interest",
+    120: "School Activities",
+    199: "Other",
+    null: "None"
+};
